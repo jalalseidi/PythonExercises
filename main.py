a436780ba91341017fb8044ec9ec0a4702f6e63b
Jalal Seidi <jalalseidi7@gmail.com>
2024-09-30 19:50:47 +0100

    For each letter in the chosen word add a (_)

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ chosen_word = random.choice(word_list)
 
 print(chosen_word)
 
+word_length = len(chosen_word)
+placeholder = ""
+
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
 guess = input("Guess a letter: ")
 print(f"You guessed: {guess.lower()}")
 # Loop through each letter in the chosen word and ask the user to guess
+
 for x in chosen_word:
 
     if guess == x:
